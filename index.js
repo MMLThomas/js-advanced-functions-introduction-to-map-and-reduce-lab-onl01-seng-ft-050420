617d444db79bf5cf6171387953f30e96d3813e54
michael thomas <michael.thomas2014@gmail.com>
2020-08-24 18:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function reduceToTotal(sourceArray, startingPoint = 0){
   return sourceArray.reduce(function(total, element){return total += element},startingPoint);
 }
 function reduceToAllTrue(sourceArray){
-  
+  return sourceArray.reduce(function(total, element){return total += element},startingPoint);
 }
 function reduceToAnyTrue(sourceArray){
-  
+  return sourceArray.reduce(function(total, element){return true}, 0);
 }
